767b9b4cc3d056eafc708b4691fe089724113bd0
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-02-23 01:17:17 +0100

    init create next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Leaf",
+  description: "Grab all the leaves and become the biggest tree!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
