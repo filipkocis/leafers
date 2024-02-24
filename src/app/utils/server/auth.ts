20ec5c3cee4d2e0762ada3f7709b5a6b245ebd6b
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-02-24 18:46:38 +0100

    feat(utils): add auth utility function

--- src/app/utils/server/auth.ts
+++ src/app/utils/server/auth.ts
@@ -10,3 +10,14 @@ export async function getSession() {
 
   return session;
 }
+
+export async function getUser() {
+  const supabase = await createClient();
+  const { data: { user }, error } = await supabase.auth.getUser();
+
+  if (error) {
+    throw error;
+  }
+
+  return user;
+}
