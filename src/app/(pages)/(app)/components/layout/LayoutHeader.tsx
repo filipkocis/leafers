18a41206663ee70b622d5145650fa49d0dbc7ca0
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-01 02:08:19 +0100

    feat(header): add simple header to app layout

---
+++ src/app/(pages)/(app)/components/layout/LayoutHeader.tsx
@@ -0,0 +1,32 @@
+import { Input } from "@shadcn/components/ui/input";
+import Image from "next/image";
+import ProfilePicture from "@app/components/ProfilePicture";
+import { getOwnProfileData } from "@app/utils/server/getProfile";
+import Error from "../Error";
+
+export default async function LayoutHeader({ height }: { height: string }) {
+  let profile = await getOwnProfileData()
+
+  if (profile.error) return <Error message={profile.error.message} /> 
+
+  return (
+    <header 
+      style={{ height }} 
+      className="backdrop-blur-md border sticky top-0 px-4 py-3 gap-6 border-b grid grid-cols-[auto_1fr_auto] items-center"
+    >
+      <div className="flex items-center gap-2">
+        <Image src="/leaf.png" alt="Leafers logo" width={40} height={40} /> 
+        <h1 className="hidden md:block text-3xl font-light">Leafers</h1>
+      </div>
+
+      <Input placeholder="Search" className="w-[20rem] max-w-full justify-self-end" />
+
+      <div className="flex items-center gap-4">
+        <ProfilePicture src={profile.data.avatar_url} alt="Profile picture" size={42} /> 
+        <p className="text-lg font-semibold">{
+          profile.data.display_name || '@' + profile.data.username
+        }</p>
+      </div>
+    </header>
+  )
+}
--- src/app/(pages)/(app)/layout.tsx
+++ src/app/(pages)/(app)/layout.tsx
@@ -1,18 +1,22 @@
 import NavItems from "@app/components/NavItems";
 import LogoutButton from "@app/components/LogoutButton";
 import NewPostButton from "@app/components/NewPostButton";
+import LayoutHeader from "@app/components/layout/LayoutHeader";
 
 export default async function Layout({ children, newPostForm }: { children: React.ReactNode, newPostForm: React.ReactNode }) {
+  const layoutHeaderHeightRem = "4.6rem"
+
   return (
     <div className="grid">
-      <div className="grid grid-rows-[auto_1fr] w-full min-h-[100dvh]">
-        {newPostForm}
-
-        <header className=""></header>
+      <div className="grid grid-rows-[auto_1fr] relative w-full min-h-[100dvh]">
+        <LayoutHeader height={layoutHeaderHeightRem} />
 
         <div className="grid grid-cols-[auto_1fr] md:grid-cols-[1fr_auto_1fr]">
           <div className="relative w-full">
-            <nav className="sticky w-full items-start flex justify-end top-0 overflow-y-auto">
+            <nav 
+              style={{ top: layoutHeaderHeightRem }} 
+              className="sticky w-full items-start flex justify-end overflow-y-auto"
+            >
               <ul className="flex flex-col gap-6 w-min h-full p-2">
                 <NavItems />
                 <NewPostButton />
@@ -21,13 +25,14 @@ export default async function Layout({ children, newPostForm }: { children: Reac
             </nav>
           </div>
 
-          <main className="grid md:w-[42rem] border">
+          <main className="grid md:w-[42rem] border-l border-r">
             {children}
           </main>
         </div>
       </div>
 
       <footer></footer>
+      {newPostForm}
     </div>
   )
 }
