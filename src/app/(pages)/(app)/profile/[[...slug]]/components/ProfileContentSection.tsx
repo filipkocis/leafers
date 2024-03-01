233b2403cf1a994c5ce90c13878c800e7f11a9ec
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-01 02:25:12 +0100

    feat(nav): implement new page validation method

--- src/app/(pages)/(app)/profile/[[...slug]]/components/ProfileContentSection.tsx
+++ src/app/(pages)/(app)/profile/[[...slug]]/components/ProfileContentSection.tsx
@@ -1,16 +1,13 @@
-import { cn } from "@/app/lib/shadcn/lib/utils";
-import Link from "next/link";
-import { RedirectType, redirect } from "next/navigation";
+import NavTabs from "@app/components/nav/NavTabs";
+import { validatePage } from "@app/utils/server/validatePage";
 
-type PageType = "home" | "posts" | "logs" | "stats"
-
-function parsePageType(page: string | undefined): PageType {
-  if (page === "posts") return page;
-  if (page === "logs") return page;
-  if (page === "stats") return page;
-  if (!page || !page.length || page === "home") return "home";
-  
-  redirect("/404", RedirectType.replace)
+function getNavItems(username: string) {
+  return [
+    { page: "home", label: "Home", href: `/profile/${username}/` },
+    { page: "posts", label: "Posts", href: `/profile/${username}/posts` },
+    { page: "logs", label: "Logs", href: `/profile/${username}/logs` },
+    { page: "stats", label: "Statistics", href: `/profile/${username}/stats` },
+  ]
 }
 
 export default function ProfileContentSection(
@@ -18,38 +15,21 @@ export default function ProfileContentSection(
   { profile: { id: string, username: string } , page: string | undefined 
 }) {
   const { id, username } = profile; 
-  const currentPage = parsePageType(page);
+  const navItems = getNavItems(username)
+
+  const currentPage = validatePage({
+    page,
+    pages: ["posts","logs","stats"] as const,
+    fallback: "home",
+    illegalRedirect: `/profile/${username}/`
+  });
 
   return (
     <div className="flex flex-col">
-      <ProfileNav username={username} currentPage={currentPage} /> 
+      <NavTabs check={(page => page === currentPage)} items={navItems} />
 
       <div>
       </div>
     </div>
-  )
-}
-
-function ProfileNav({ username, currentPage }: { username: string, currentPage: PageType }) {
-  return (
-    <div className="grid grid-cols-[repeat(4,_auto)] border-b">
-      <ProfileNavItem selected={currentPage === "home"} username={username} page="" label="Home" />
-      <ProfileNavItem selected={currentPage === "posts"} username={username} page="posts" label="Posts" />
-      <ProfileNavItem selected={currentPage === "logs"} username={username} page="logs" label="Logs" />
-      <ProfileNavItem selected={currentPage === "stats"} username={username} page="stats" label="Statistics" />
-    </div>
-  )
-}
-
-function ProfileNavItem({ username, page, label, selected }: { selected: boolean, username: string, page: string, label: string }) {
-  return (
-    <Link 
-      className="transition-all hover:bg-slate-200/50 flex justify-center font-semibold text-muted-foreground" 
-      href={`/profile/${username}/${page}`}>
-      <div className="grid grid-rows-[1fr_5px]">
-        <span className={cn("py-3", selected && "text-foreground")}>{label}</span>
-        <div className={cn("rounded-full", selected && "bg-primary")}></div>
-      </div>
-    </Link>
   )
 }
