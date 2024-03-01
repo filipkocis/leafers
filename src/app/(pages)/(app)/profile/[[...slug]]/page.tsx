c1b309ca860cc9012174afbe60a1ef5f28bb18dd
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-01 02:23:30 +0100

    feat(error): implement error handling

--- src/app/(pages)/(app)/profile/[[...slug]]/page.tsx
+++ src/app/(pages)/(app)/profile/[[...slug]]/page.tsx
@@ -1,29 +1,31 @@
 import { RedirectType, redirect } from "next/navigation";
-import { getProfileData, getProfileDataByUsername } from "./utils/getProfileData";
+import { getProfileDataByUsername } from "@app/utils/server/getProfile";
+import { getOwnProfileData } from "@app/utils/server/getProfile";
 import ProfileContentSection from "./components/ProfileContentSection";
 import ProfileHeaderSection from "./components/ProfileHeaderSection";
 
-export default async function Profile({ params: { slug } }: { params: { slug: string[] } }) {
-  if (!slug || slug.length === 0) {
-    const profileSelf = await getProfileData(); // TODO: rename
-    if (!profileSelf) redirect("/login", RedirectType.replace)
+export default async function Profile({ params: { slug } }: { params: { slug?: string[] } }) {
+  if (slug && slug.length > 2) {
+    redirect("/404", RedirectType.replace)
+  }
 
-    const username = profileSelf.username
+  if (!slug || slug.length === 0) {
+    const profile = await getOwnProfileData();
+    if (profile.error) redirect("/login", RedirectType.replace)
+
+    const username = profile.data.username
     redirect(`/profile/${username}`, RedirectType.replace)
   }
 
   const [username, page] = slug
 
-  const profile = await getProfileDataByUsername(username); // TODO: rename
-
-  if (!profile) {
-    redirect("/explore", RedirectType.replace)
-  }
+  const profile = await getProfileDataByUsername(username);
+  if (profile.error) redirect("/404", RedirectType.replace)
 
   return (
     <div className="flex flex-col gap-6">
-      <ProfileHeaderSection profile={profile} />
-      <ProfileContentSection profile={profile} page={page} />
+      <ProfileHeaderSection profile={profile.data} />
+      <ProfileContentSection profile={profile.data} page={page} />
     </div>
   )
 }
