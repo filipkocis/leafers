d216140a1f092aded9730c8f7568de06e1f6751f
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-01 01:47:54 +0100

    feat(utils): add utility functions for formatting

---
+++ src/app/utils/format.ts
@@ -0,0 +1,38 @@
+import { formatDistanceToNow } from "date-fns";
+import { UnitEnum } from "@app/utils/post";
+
+export function formatPostDate(date: string) {
+  const dateObj = new Date(date)
+  const formated = formatDistanceToNow(dateObj, { addSuffix: true });
+
+  return formated.replace("about ", "")
+}
+
+export function formatUnit(unit: UnitEnum) {
+  switch (unit) {
+    case 'gram': return 'g';
+    case 'miligram': return 'mg';
+    case 'second': return 's';
+    case 'hour': return 'h';
+    default: return unit;
+  }
+}
+
+const amountFormatterCompact = new Intl.NumberFormat("en-US", { 
+  compactDisplay: "short",
+  notation: "compact",
+  maximumSignificantDigits: 3,
+})
+
+const amountFormatterScientific = new Intl.NumberFormat("en-US", { 
+  compactDisplay: "short",
+  notation: "scientific",
+  maximumSignificantDigits: 3,
+})
+
+export function formatAmount(number?: number) {
+  if (number == undefined) return null;
+  return number < 10**15 ? 
+    amountFormatterCompact.format(number) :
+    amountFormatterScientific.format(number)
+}
