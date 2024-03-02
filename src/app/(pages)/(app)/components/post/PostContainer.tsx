df12a113ec1b91e451a60baa968ce73d55980a3a
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-02 23:35:37 +0100

    style: change y-padding to smaller number

--- src/app/(pages)/(app)/components/post/PostContainer.tsx
+++ src/app/(pages)/(app)/components/post/PostContainer.tsx
@@ -3,7 +3,7 @@ import { cn } from "@shadcn/lib/utils";
 
 export default function PostContainer({ children, className }: { children: React.ReactNode, className?: string }) {
   return (
-    <Card className={cn("px-4 py-3   rounded-none bg-transparent", className)}>
+    <Card className={cn("px-4 py-2 rounded-none bg-transparent", className)}>
       {children}
     </Card>
   )
