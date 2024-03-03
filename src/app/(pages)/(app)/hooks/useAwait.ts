233606d7b81c02d3e2160163ccc4fe7b92c661cc
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-03 17:34:45 +0100

    feat: implement generic  useAwait hooks

---
+++ src/app/(pages)/(app)/hooks/useAwait.ts
@@ -0,0 +1,60 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+type ReturnType<T> = {
+  error: string,
+  data: null,
+  loading: false,
+} | {
+  data: T,
+  error: null,
+  loading: false,
+} | {
+  loading: true,
+  error: null,
+  data: null,
+}
+
+export function useAwait<T>(fn: (...params: any) => Promise<T>, ...params: any): ReturnType<T> {
+  const [data, setData] = useState<Awaited<T> | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const awaitData = async () => {
+      console.log("fetching")
+      try {
+        const result = await fn(...params);
+        setData(result);
+      } catch (error: any) {
+        const msg = `${error?.message}` || "An error occurred";
+        setError(msg);
+      } 
+      setLoading(false);
+    }
+    awaitData();
+  }, [fn, ...params])
+
+  if (loading === true) return { data: null, error: null, loading: true as const }
+  if (error != null) return { data: null, error, loading: false as const }
+  return { data: data as T, error: null, loading: false as const }
+}
+
+type ReturnObject<T> = {
+  data: T,
+  error: undefined | null
+} | {
+  error: {
+    message: string
+  } 
+}
+
+export function useAwaitData<T>(fn: (...params: any) => Promise<ReturnObject<T>>, ...params: any): ReturnType<T> {
+  const { data, error, loading } = useAwait(fn, ...params);
+
+  if (loading) return { data: null, error: null, loading: true as const }
+  if (error != null) return { data: null, error, loading: false as const }
+  if (data.error) return { data: null, error: data.error.message, loading: false as const }
+  return { data: data.data as T, error: null, loading: false as const }
+}
