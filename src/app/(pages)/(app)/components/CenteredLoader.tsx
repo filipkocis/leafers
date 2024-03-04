a6aaa8008addeba26de7d3ca3f0181040ecd9d3f
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-04 01:11:50 +0100

    fix: fix loader size based on params

--- src/app/(pages)/(app)/components/CenteredLoader.tsx
+++ src/app/(pages)/(app)/components/CenteredLoader.tsx
@@ -1,13 +1,13 @@
 import { cn } from "@shadcn/lib/utils";
 import { Loader2 } from "lucide-react";
 
-export default function CenteredLoader({ className, size }: { className?: string, size?: string }) {
+export default function CenteredLoader({ className, size = 28 }: { className?: string, size?: string | number }) {
   return (
     <div className="flex items-center justify-center h-full">
       <Loader2 
         width={size} 
         height={size} 
-        className={cn("animate-spin fade-in rounded-full h-10 w-10 text-muted-foreground", className)} 
+        className={cn("animate-spin fade-in rounded-full text-muted-foreground", className)} 
       />
     </div>
   )
