d2c23767fd1635c5d3d893b9ae43f93de4377d76
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-12 13:27:57 +0100

    feat(feed): add memo to linkpost for feed optimization

--- src/app/(pages)/(app)/features/post/LinkPost.tsx
+++ src/app/(pages)/(app)/features/post/LinkPost.tsx
@@ -2,11 +2,14 @@
 
 import Link from "next/link";
 import Post from "@app/features/post/Post";
+import React from "react";
 
-export default function LinkPost({ post, className }: { post: any, className?: string }) {
+const LinkPost = React.memo(({ post, className }: { post: any, className?: string }) => {
   return (
     <Link href={`/post/${post.id}`} className="hover:bg-muted/30">
       <Post post={post} className={className} />
     </Link>
   )
-}
+})
+
+export default LinkPost
