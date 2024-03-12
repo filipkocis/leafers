033dbe9f3eefa33d294edd685af54263ea519df5
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-12 13:35:52 +0100

    feat(post): add link for profile username

--- src/app/(pages)/(app)/features/post/components/PostUsername.tsx
+++ src/app/(pages)/(app)/features/post/components/PostUsername.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@shadcn/lib/utils";
+import Link from "next/link";
 
 export default function PostUsername({ username, displayName }: { username: string, displayName?: string | null }) {
   return (
@@ -6,7 +7,15 @@ export default function PostUsername({ username, displayName }: { username: stri
       {!!displayName &&
         <p className={cn("overflow-hidden text-ellipsis whitespace-nowrap font-bold text-[1rem]")}>{displayName}</p>
       }
-      <p className={cn("overflow-hidden text-ellipsis", displayName ? "text-muted-foreground" : "font-bold text-[1rem]")}>@{username}</p>
+      <Link 
+        href={`/profile/${username}`} 
+        className={cn(
+          "overflow-hidden text-ellipsis hover:underline ", 
+          displayName ? "text-muted-foreground" : "font-bold text-[1rem]"
+        )}
+      >
+        @{username}
+      </Link>
     </div>
   )
 }
