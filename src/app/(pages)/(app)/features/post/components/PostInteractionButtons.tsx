fdb3f39f36b5ad2e61a138f9c4bf0b2283430d06
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-12 16:41:22 +0100

    fix(post): stop buttons from redirecting to the post page

--- src/app/(pages)/(app)/features/post/components/PostInteractionButtons.tsx
+++ src/app/(pages)/(app)/features/post/components/PostInteractionButtons.tsx
@@ -1,19 +1,39 @@
 "use client"
 
+import { Button } from "@/app/lib/shadcn/components/ui/button";
 import { LucideHeart, LucideReplyAll, LucideShare } from "lucide-react";
 
 export default function PostInteractionButtons({ id }: { id: string }) {
   return (
     <div className="col-span-2 flex justify-around [&>*]:p-1 [&>*]:rounded-full"> 
-      <div className="hover:text-red-500 hover:bg-red-500/25 hover:shadow-[0_0_0_0.2rem_rgb(239_68_68_/_0.25)]">
-        <LucideHeart size={16} />
-      </div>
-      <div className="hover:text-primary hover:bg-primary/25 hover:shadow-[0_0_0_0.2rem_hsl(var(--primary)_/_25%)]">
-        <LucideReplyAll size={16} />
-      </div>
-      <div className="hover:text-primary hover:bg-primary/25 hover:shadow-[0_0_0_0.2rem_hsl(var(--primary)_/_25%)]">
-        <LucideShare size={16} />
-      </div>
+      <ButtonWrapper>
+        <div className="hover:text-red-500 hover:bg-red-500/25 hover:shadow-[0_0_0_0.2rem_rgb(239_68_68_/_0.25)]">
+          <LucideHeart size={16} />
+        </div>
+      </ButtonWrapper>
+      <ButtonWrapper>
+        <div className="hover:text-primary hover:bg-primary/25 hover:shadow-[0_0_0_0.2rem_hsl(var(--primary)_/_25%)]">
+          <LucideReplyAll size={16} />
+        </div>
+      </ButtonWrapper>
+      <ButtonWrapper>
+        <div className="hover:text-primary hover:bg-primary/25 hover:shadow-[0_0_0_0.2rem_hsl(var(--primary)_/_25%)]">
+          <LucideShare size={16} />
+        </div>
+      </ButtonWrapper>
     </div>
+  )
+}
+
+function ButtonWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <Button 
+      variant="ghost" 
+      className="h-auto transition-all" 
+      asChild 
+      onClick={(e) => e.preventDefault()}
+    >
+      {children}
+    </Button>
   )
 }
