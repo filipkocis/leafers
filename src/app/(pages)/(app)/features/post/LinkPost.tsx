1201e4c191e3e5c4a527b7ac4a099d9c298913a0
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-12 16:32:55 +0100

    refactor(post): replace link component to div with router to allow link nesting

--- src/app/(pages)/(app)/features/post/LinkPost.tsx
+++ src/app/(pages)/(app)/features/post/LinkPost.tsx
@@ -1,14 +1,16 @@
 "use client"
 
-import Link from "next/link";
 import Post from "@app/features/post/Post";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const LinkPost = React.memo(({ post, className }: { post: any, className?: string }) => {
+  const router = useRouter()
+
   return (
-    <Link href={`/post/${post.id}`} className="hover:bg-muted/30">
+    <div onClick={() => router.push(`/post/${post.id}`)} className="cursor-pointer hover:bg-muted/30">
       <Post post={post} className={className} />
-    </Link>
+    </div>
   )
 })
 
