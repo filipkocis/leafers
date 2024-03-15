0292aa3001ed55690c3588b31208b0c21aadfda3
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-15 15:25:58 +0100

    feat(post): implement generic post button to stop redirect propagation

---
+++ src/app/(pages)/(app)/features/post/components/PostButton.tsx
@@ -0,0 +1,18 @@
+import { Button, ButtonProps } from "@shadcn/components/ui/button"
+import { forwardRef } from "react"
+
+export const PostButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ onClick, ...props }, ref) => {
+    return (
+      <Button
+        ref={ref}
+        onClick={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          if (onClick) onClick(e)
+        }}
+        {...props}
+      />
+    )
+  }
+)
