7c1491526dfd8f148c2a90a1bd36d08fc60559ca
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-30 17:37:00 +0100

    feat(getPostById): implement counts

--- src/app/(pages)/(app)/post/utils/server/getPostById.ts
+++ src/app/(pages)/(app)/post/utils/server/getPostById.ts
@@ -1,5 +1,6 @@
 import createClient from "@services/supabase/server";
 import { dataNonNull, errorMessage } from "@utils/returnObjects";
+import { getNumberFromSqlCount } from "@app/utils/helpers";
 
 export async function getPostById(id: string) {
   try {
@@ -7,14 +8,21 @@ export async function getPostById(id: string) {
     
     let { data, error } = await supabase
       .from('posts')
-      .select('*, profiles!inner(id, username, display_name, avatar_url)')
+      .select('*, profiles!inner(id, username, display_name, avatar_url), replies_count:posts!parent_id(count), likes_count:likes(count), reposts_count:reposts!post_id(count)')
       .eq('id', id)
       .single()
 
     if (error) throw error
     if (!data) throw new Error("No post found")
 
-    return dataNonNull(data)
+    const mapped = {
+      ...data,
+      likes_count: getNumberFromSqlCount(data.likes_count),
+      replies_count: getNumberFromSqlCount(data.replies_count),
+      reposts_count: getNumberFromSqlCount(data.reposts_count),
+    }
+
+    return dataNonNull(mapped)
   } catch (error) {
     console.error(error)
     return errorMessage(error, "Failed to load post")
