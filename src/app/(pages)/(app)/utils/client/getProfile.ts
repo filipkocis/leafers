a463cffa2996b5f419e42e636842f8c3f6da73ec
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-03-30 18:22:05 +0100

    refactor(getProfile): implement client version

---
+++ src/app/(pages)/(app)/utils/client/getProfile.ts
@@ -0,0 +1,46 @@
+"use client"
+
+import createClient from "@/app/services/supabase/client";
+import { getUser } from "@/app/utils/client/auth";
+import { dataNonNull, errorMessage } from "@/app/utils/returnObjects";
+
+export async function getOwnProfileId() {
+  try {
+    const supabase = createClient();
+    const user = await getUser();
+
+    if (!user) throw new Error("User not found");
+
+    const { data: profile, error } = await supabase
+      .from("profiles")
+      .select("id")
+      .eq("auth_user_id", user.id)
+      .single();
+
+    if (error) throw error
+    if (!profile) throw new Error("Profile not found")
+
+    return dataNonNull(profile.id);
+  } catch (error) {
+    return errorMessage(error, "Could not get profile id")
+  }
+}
+
+export async function getProfileDataByUsername(username: string) {
+  try {
+    const supabase = createClient();
+
+    const { data: profile, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("username", username)
+      .single();
+
+    if (error) throw error;
+    if (!profile) throw new Error("Profile not found")
+
+    return dataNonNull(profile);
+  } catch (error) {
+    return errorMessage(error, "Could not get profile data")
+  }
+}
