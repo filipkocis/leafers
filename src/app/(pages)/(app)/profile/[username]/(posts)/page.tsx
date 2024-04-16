c48af9121b1fcf0bf731a5e3936b1cc6096dfb3c
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-04-16 15:44:16 +0200

    feat(profile): implement home,logs,replies feed

--- src/app/(pages)/(app)/profile/[username]/(posts)/page.tsx
+++ src/app/(pages)/(app)/profile/[username]/(posts)/page.tsx
@@ -1,5 +1,10 @@
-export default function PostsPage() {
-  return (
-  <div>posts page</div>
-  )
+import Error from "@app/components/Error"
+import { getProfileDataByUsername } from "@app/utils/server/getProfile"
+import ServerFeedWrapper from "@app/components/ServerFeedWrapper";
+
+export default async function PostsPage({ params: { username } }: { params: { username: string } }) {
+  const { data: profile, error } = await getProfileDataByUsername(username);
+  if (error) return <Error message={error.message} />
+
+  return <ServerFeedWrapper profileId={profile.id} />
 }
--- src/app/(pages)/(app)/profile/[username]/logs/page.tsx
+++ src/app/(pages)/(app)/profile/[username]/logs/page.tsx
@@ -1,5 +1,10 @@
-export default function LogsPage() {
-  return (
-  <div>logs page</div>
-  )
+import Error from "@app/components/Error";
+import ServerFeedWrapper from "@app/components/ServerFeedWrapper";
+import { getProfileDataByUsername } from "@app/utils/server/getProfile";
+
+export default async function LogsPage({ params: { username } }: { params: { username: string } }) {
+  const { data: profile, error } = await getProfileDataByUsername(username);
+  if (error) return <Error message={error.message} />
+
+  return <ServerFeedWrapper profileId={profile.id} type="log" />
 }
