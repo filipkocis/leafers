2653e6e055155351883242d83a8d008bba882a07
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-04-16 15:42:42 +0200

    refactor(explore): implement feed wrapper

--- src/app/(pages)/(app)/home/[[...slug]]/components/ExplorePosts.tsx
+++ src/app/(pages)/(app)/home/[[...slug]]/components/ExplorePosts.tsx
@@ -1,15 +1,5 @@
-import { getPaginatedPosts } from "@app/utils/server/getPosts"
-import Error from "@app/components/Error"
-import InfinitePostFeed from "@app/components/InfinitePostFeed"
+import ServerFeedWrapper from "@app/components/ServerFeedWrapper"
 
 export default async function ExplorePosts() {
-  const { data: posts, error } = await getPaginatedPosts({ limit: 10, offset: 0 })
-
-  if (error) return <Error message={error.message} />
-
-  return (
-    <div className="grid">
-      <InfinitePostFeed defaultPosts={posts} />
-    </div>
-  )
+  return <ServerFeedWrapper />
 }
