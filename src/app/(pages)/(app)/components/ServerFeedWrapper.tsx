81d1f988e9d0d0fabfc5567e9045dbb3e269e835
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-04-16 15:40:32 +0200

    feat(feed): add generic server side feed wrapper

---
+++ src/app/(pages)/(app)/components/ServerFeedWrapper.tsx
@@ -0,0 +1,28 @@
+import Error from "@app/components/Error"
+import InfinitePostFeed from "@app/components/InfinitePostFeed"
+import { getPaginatedPosts } from "@app/utils/server/getPosts"
+import { PostTypeEnum } from "@app/utils/types"
+
+export default async function ServerFeedWrapper({ 
+  profileId, parentId, type  
+}: {
+  profileId?: string,
+  parentId?: string,
+  type?: "reply" | PostTypeEnum
+}) {
+  const { data: posts, error: error } = await getPaginatedPosts({ 
+    limit: 10,
+    offset: 0,
+    type: type,
+    profile_id: profileId,
+    parent_id: parentId,
+  })
+
+  if (error) return <Error message={error.message} />
+
+  return (
+    <div className="grid">
+      <InfinitePostFeed type={type} profileId={profileId} parentId={parentId} defaultPosts={posts} />
+    </div>
+  )
+}
