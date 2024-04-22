ae256e4e707f04e3c98689850df81ee173aeafd1
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-04-22 17:13:02 +0200

    feat(profile): add role guards for sub pages

--- src/app/(pages)/(app)/profile/[username]/logs/page.tsx
+++ src/app/(pages)/(app)/profile/[username]/logs/page.tsx
@@ -1,8 +1,11 @@
+import { hasRole } from "@utils/server/roles";
 import Error from "@app/components/Error";
 import ServerFeedWrapper from "@app/components/ServerFeedWrapper";
 import { getProfileDataByUsername } from "@app/utils/server/getProfile";
 
 export default async function LogsPage({ params: { username } }: { params: { username: string } }) {
+  if (!await hasRole("leafer")) return <Error message="You do not have permission to view this page" />
+
   const { data: profile, error } = await getProfileDataByUsername(username);
   if (error) return <Error message={error.message} />
 
--- src/app/(pages)/(app)/profile/[username]/stats/page.tsx
+++ src/app/(pages)/(app)/profile/[username]/stats/page.tsx
@@ -1,4 +1,9 @@
-export default function StatsPage() {
+import { hasRole } from "@utils/server/roles"
+import Error from "@app/components/Error"
+
+export default async function StatsPage() {
+  if (!await hasRole("leafer")) return <Error message="You do not have permission to view this page" />
+
   return (
   <div>stats page</div>
   )
