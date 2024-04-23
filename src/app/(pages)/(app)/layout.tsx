a1d327143a54bcec35c91a0333093db9d82826ed
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-04-23 13:29:03 +0200

    feat(layout): refactor and add mobile version

--- src/app/(pages)/(app)/layout.tsx
+++ src/app/(pages)/(app)/layout.tsx
@@ -1,53 +1,63 @@
 import NewPostButton from "@app/components/NewPostButton";
 import LayoutHeader from "@app/components/layout/LayoutHeader";
-import ProfileIdProvider from "@app/contexts/profileIdContext";
-import { getOwnProfileId } from "@app/utils/server/getProfile";
+import ProfileProvider from "@app/contexts/profileContext";
+import { getOwnProfileData } from "@app/utils/server/getProfile";
 import { RedirectType, redirect } from "next/navigation";
 import SideNavItems from "@app/components/nav/SideNavItems";
 import { isAdmin } from "@utils/server/roles";
-import { ThemeToggle } from "@/app/components/ThemeToggle";
+import { ThemeToggle } from "@components/ThemeToggle";
+import OnlyDesktop from "@app/components/OnlyDesktop";
 
 export default async function Layout({ children, newPostForm }: { children: React.ReactNode, newPostForm: React.ReactNode }) {
-  const { data: profileId, error: profileIdError } = await getOwnProfileId()
-  if (profileIdError) redirect('/', RedirectType.replace)
+  const { data: profile, error: profileError } = await getOwnProfileData()
+  if (profileError) redirect('/', RedirectType.replace)
 
   const adminPrivileges = await isAdmin()
   const layoutHeaderHeightRem = "4.6rem"
 
   return (
     <div className="grid">
-      <div className="grid grid-rows-[auto_1fr] relative w-full min-h-[100dvh]">
-        <LayoutHeader height={layoutHeaderHeightRem} />
+      <ProfileProvider value={profile}>
+        <div className="grid grid-rows-[auto_1fr] relative w-full min-h-[100dvh]">
+          <LayoutHeader height={layoutHeaderHeightRem}>
+            <ul className="overflow-x-hidden overflow-ellipsis grid grid-rows-[auto,auto,1fr] gap-2 h-full">
+              <SideNavItems adminPrivileges={adminPrivileges} />
+              <NewPostButton />
+              {newPostForm}
+              <ThemeToggle className="self-end place-self-center" />
+            </ul>
+          </LayoutHeader>
 
-        <div className="grid grid-cols-[auto_1fr] md:grid-cols-[1fr_auto_1fr]">
-          <div className="relative w-full">
-            <nav 
-              style={{ top: layoutHeaderHeightRem }} 
-              className="sticky w-full items-start flex justify-end overflow-y-auto"
-            >
-              <ul 
-                style={{ 
-                  height: `calc(100dvh - ${layoutHeaderHeightRem})` 
-                }}
-                className="grid grid-rows-[auto,auto,1fr] gap-6 w-min h-full p-2"
-              >
-                <SideNavItems adminPrivileges={adminPrivileges} />
-                <NewPostButton />
-                {newPostForm}
-                <ThemeToggle className="self-end place-self-center" />
-              </ul>
-            </nav>
+          <div className="grid grid-cols-[auto_1fr] md:grid-cols-[1fr_auto_1fr]">
+            <div className="relative w-full">
+              <OnlyDesktop>
+                <nav 
+                  style={{ top: layoutHeaderHeightRem }} 
+                  className="max-sm:hidden sticky w-full items-start flex justify-end overflow-y-auto"
+                >
+                  <ul 
+                    style={{ 
+                      height: `calc(100dvh - ${layoutHeaderHeightRem})` 
+                    }}
+                    className="grid grid-rows-[auto,auto,1fr] gap-6 w-min h-full p-2"
+                  >
+                    <SideNavItems adminPrivileges={adminPrivileges} />
+                    <NewPostButton />
+                    {newPostForm}
+                    <ThemeToggle className="self-end place-self-center" />
+                  </ul>
+                </nav>
+              </OnlyDesktop>
+            </div>
+
+              <main className="grid md:w-[42rem] border-l border-r">
+                {children}
+              </main>
           </div>
-
-          <ProfileIdProvider value={profileId}>
-            <main className="grid md:w-[42rem] border-l border-r">
-              {children}
-            </main>
-          </ProfileIdProvider>
         </div>
-      </div>
 
-      <footer></footer>
+        <footer></footer>
+      </ProfileProvider>
     </div>
   )
 }
