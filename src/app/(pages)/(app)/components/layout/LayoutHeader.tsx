96452ee1cad8b16d24a1cfdf4f4ee05845cbaa38
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-04-23 13:27:30 +0200

    feat(header): implement mobile version

--- src/app/(pages)/(app)/components/layout/LayoutHeader.tsx
+++ src/app/(pages)/(app)/components/layout/LayoutHeader.tsx
@@ -1,32 +1,41 @@
+"use client"
+
 import { Input } from "@shadcn/components/ui/input";
 import Image from "next/image";
-import { getOwnProfileData } from "@app/utils/server/getProfile";
-import Error from "@app/components/Error";
 import { LayoutHeaderProfileDropdown } from "./LayoutHeaderProfileDropdown";
 import Link from "next/link";
+import { LayoutHeaderSheet } from "./LayoutHeaderSheet";
+import { useIsMobile } from "@app/contexts/IsMobileContext";
 
-export default async function LayoutHeader({ height }: { height: string }) {
-  let profile = await getOwnProfileData()
+export default function LayoutHeader({ height, children }: { height: string, children?: React.ReactNode }) {
+  const isMobile = useIsMobile()
 
-  if (profile.error) return <Error message={profile.error.message} /> 
+  if (isMobile && children === undefined) return null;
+  if (isMobile) return (
+    <header className="z-50 backdrop-blur-md sticky top-0 px-4 py-3 gap-6 border-b grid grid-cols-[auto_1fr_auto] items-center">
+      <LayoutHeaderSheet>{children}</LayoutHeaderSheet>
+
+      <div className="flex justify-center">
+        <Image src="/leaf.png" alt="Leafers logo" width={40} height={40} /> 
+      </div>
+
+      <LayoutHeaderProfileDropdown />
+    </header>
+  )
 
   return (
     <header 
       style={{ height }} 
       className="z-50 backdrop-blur-md sticky top-0 px-4 py-3 gap-6 border-b grid grid-cols-[auto_1fr_auto] items-center"
     >
-      <Link href="/" className="flex items-center gap-2">
+      <Link href="/" className="max-sm:invisible flex items-center gap-2">
         <Image src="/leaf.png" alt="Leafers logo" width={40} height={40} /> 
         <h1 className="hidden md:block text-3xl font-light">Leafers</h1>
       </Link>
 
-      <Input placeholder="Search" className="w-[20rem] max-w-full justify-self-end" />
+      <Input disabled placeholder="Search" className="max-sm:invisible sm:w-[20rem] max-w-full justify-self-end" />
 
-      <LayoutHeaderProfileDropdown 
-        username={profile.data.username} 
-        displayName={profile.data.display_name}
-        avatarUrl={profile.data.avatar_url}
-      />
+      <LayoutHeaderProfileDropdown />
     </header>
   )
 }
