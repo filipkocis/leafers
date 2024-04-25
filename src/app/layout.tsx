ba63c4c6ee191d85b447e2db9005ec488bae1ddf
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-04-25 11:57:22 +0200

    fix(sonner): move toaster from html to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,9 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </IsMobileProvider>
-      </body>
 
-      <Toaster richColors />
+        <Toaster richColors />
+      </body>
     </html>
   );
 }
