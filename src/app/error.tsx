98c9ee789a89973c3dbc7822f18cb9130ede3f99
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-07-07 02:33:27 +0200

    fix(crash): fix initial error with capped reloads

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { Button } from "@shadcn/components/ui/button"
-
+import { toast } from "sonner"
+import { wait } from "./utils/wait"
+import { useEffect } from "react"
  
 export default function Error({
   error,
@@ -10,6 +12,39 @@ export default function Error({
   error: Error & { digest?: string }
   reset: () => void
 }) {
+  useEffect(() => {
+    let flag = { ignore: false }
+    toast.info('Reloading in 1 second', { duration: 750 })
+
+    async function reload() {
+      await wait(1000)
+      if (flag.ignore) return
+
+      if (window.location.pathname === '/home') {
+        const hash = window.location.hash
+
+        if (hash && hash !== '#') {
+          const count = parseInt(hash.slice(1))
+          if (count <= 5) window.location.hash = `#${count + 1}`
+          else {
+            toast.error('Failed to reload automatically', { duration: 1000 })
+            return
+          }
+        } else {
+          window.location.hash = '#1'
+        }
+
+        reset(); 
+        window.location.reload();
+      }
+    }
+    reload()
+
+    return () => {
+      flag.ignore = true
+    }
+  })
+
   return (
     <div className="px-3 py-2 flex gap-2 flex-col items-center">
       <h2 className="text-xl">Something went wrong!</h2>
